92fcc4ecbef2cc801240e840b3e43773bac0d4a9
Emrah Bayar <emrah.bayar.98@gmail.com>
2025-09-12 17:15:24 +0300

    Fotoğraf yükleme işlemleri hazırlandı.

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { useRef, useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const fileInputRef = useRef(null);
+  const [fileNames, setFileNames] = useState([]);
 
+  const handleButtonClick = () => {
+    fileInputRef.current.click();
+  };
+  const handleFileChange = (event) => {
+    const files = Array.from(event.target.files);
+    const names = files.map((file) => file.name); // sadece isimlerini alıyoruz
+    setFileNames(names);
+  };
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='container text-center'>
+      <div className="row d-flex justify-content-center align-items-center vh-100">
+        <div className="col-lg-12 col-md-6 col-sm">
+          <h1 className='h1 mt-3'>Düğünümüze hoşgeldiniz!</h1>
+          <h1 className='h1'>Berna & Barkın</h1>
+          <p className='mt-3'>Bizimle paylaşmak istediğiniz fotoğraflarınızı bekliyoruz.</p>
+          <div className='d-flex flex-column align-items-center'>
+            <button onClick={handleButtonClick} className='btn btn-success mb-3'>Fotoğraf paylaş</button>
+            <input
+              type="file"
+              accept="image/*"
+              ref={fileInputRef}
+              style={{ display: "none" }}
+              onChange={handleFileChange}
+              multiple
+            />
+            {fileNames.length > 0 && (
+              <ul className="list-group mb-3">
+                {fileNames.map((name, index) => (
+                  <li key={index} className="list-group-item">
+                    {name}
+                  </li>
+                ))}
+              </ul>
+            )}
+            <button className='btn btn-secondary'>Yükle</button>
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </div>
   )
 }
 
